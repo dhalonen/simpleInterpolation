3e8704399ae313ccfcafe926253a04c8e6fd1be8
David Halonen <dhalonen@Davids-MacBook-Pro.local>
2017-03-04 15:05:10 -0500

    Time to publish!

---
+++ simpleInterpolation.h
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2017 David C. Halonen  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to use, 
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Software, and to permit persons to whom the Software is furnished to do so, subject 
+ * to the following conditions: 
+ *
+ *   The above copyright notice and this permission notice shall be included 
+ *   in all copies or substantial portions of the Software.  
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#include <vector>
+#include <tuple>
+#include <cfenv>
+#include <memory>
+//This pragma allows for divide by zero & overflow testing
+#pragma STDC FENV_ACCESS ON
+
+namespace simpleTools
+{
+    template <class X, class Y>
+    class interpolation {
+    public:
+        interpolation( std::shared_ptr< std::vector< std::pair< X, X > > >a ): intrpData( a ) {};
+
+        //given interpolation point, x, compute it's corresponding y value
+        X getY( X x )
+        {
+            //If less then 2 pairs, then nothing can be done.
+            if( intrpData->size() < 2 ) return nan( "NAN" );
+
+            X leftX, leftY, rightX = nan( "NAN" ), rightY;  //rightX assignment is to provide check later
+            auto head = intrpData->begin();
+            auto end = intrpData->end();
+            if( head == end ) return nan( "NAN" );  //empty vector
+
+            leftX = head->first;                    //start from left side of graph or top of table
+            leftY = head->second;
+
+            //if leftX > x, then the interpolation point is to the left of the table.
+            //compute y = mx + b, using the 1st two pairs to determine that equation
+            if( leftX > x ){
+                ++head;
+                rightX = head->first;
+                rightY = head->second;
+                return interpolateOnSegment( x, leftX, leftY, rightX, rightY );
+            }
+
+            //scan pairs to determine where the desired point lies between
+            for( std::pair< X, Y >&item : *intrpData ){
+                if( x == leftX ) return leftY;
+                if( x == rightX ) return rightY;
+                
+                if( item.first > x ){    //we have the rhs
+                    rightX = item.first;
+                    rightY = item.second;
+                    break;
+                } else {
+                    leftX = item.first;
+                    leftY = item.second;
+                }
+            }
+
+            //if rightX <  x, then the interpolation point is to the right of the table.
+            //compute y = mx + b, using the last two pairs to determine that equation
+            if( std::isnan( rightX )){
+                //perform a reverse iteration.
+                auto rhead = intrpData->rbegin();
+                rightX = rhead->first;
+                rightY = rhead->second;
+                ++rhead;
+                leftX = rhead->first;
+                leftY = rhead->second;
+                return interpolateOnSegment( x, leftX, leftY, rightX, rightY );
+            }
+
+            //simply perform linear interpolation between two points
+            return interpolate( x, leftX, leftY, rightX, rightY );
+        }
+
+    private:
+        std::shared_ptr< std::vector< std::pair< X, X > > > intrpData;
+
+        X interpolate( X x, X leftX, X leftY, X rightX, X rightY )
+        {
+            X slope = computeSlope( leftX, leftY, rightX, rightY );
+            if( std::isnan( slope )) return slope;
+
+            std::feclearexcept(FE_ALL_EXCEPT);
+            X result = leftY + (x - leftX) * slope;
+            if(std::fetestexcept(FE_OVERFLOW)) return nan( "NAN" ); 
+
+            return result; 
+        };
+
+        X interpolateOnSegment( X x, X leftX, X leftY, X rightX, X rightY )
+        {   // y = mx + b
+            X m = computeSlope( leftX, leftY, rightX, rightY );
+            if( std::isnan( m )) return m;
+            X b = leftY - m * leftX;
+
+            std::feclearexcept(FE_ALL_EXCEPT);
+            X result = m * x + b;
+            if(std::fetestexcept(FE_OVERFLOW)) return nan( "NAN" ); 
+
+            return result; 
+        };
+
+        X computeSlope( X leftX, X leftY, X rightX, X rightY )
+        {
+            X denominator = rightX - leftX; 
+
+            std::feclearexcept(FE_ALL_EXCEPT);
+            X result = ( rightY - leftY ) / denominator;
+            if(std::fetestexcept(FE_DIVBYZERO)) return nan( "NAN" ); 
+
+            return result; 
+        }
+    };
+};
---
+++ simpleInterpolationTest.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017 David C. Halonen  
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to use, 
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Software, and to permit persons to whom the Software is furnished to do so, subject 
+ * to the following conditions: 
+ *
+ *   The above copyright notice and this permission notice shall be included 
+ *   in all copies or substantial portions of the Software.  
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#define CATCH_CONFIG_MAIN
+#include <vector>
+#include <tuple>
+#include <memory>
+
+/* Unit Test Framework: https://github.com/philsquared/Catch, release v1.8.2 */
+#include "catch.hpp"
+
+#include "simpleInterpolation.h"
+
+TEST_CASE( "First test" )
+{
+    std::shared_ptr< std::vector< std::pair< double, double > > > graphData ( new std::vector< std::pair< double, double > > ( { {1.0, 1.0}, {2.5, 1.3}, {3.0, 2.0}, {3.75, 0.5}, {4.1, 2.25}, {5.0, 1.75}, {5.3, 1.9}}));
+    simpleTools::interpolation <double, double> graphDataIntrp( graphData  );
+    REQUIRE( graphDataIntrp.getY( 1.75 ) == Approx( 1.15 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 2.75 ) == Approx( 1.65 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 3.375 ) == Approx( 1.25 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 3.925 ) == Approx( 1.375 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 4.55 ) == Approx( 2.0 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 5.15 ) == Approx( 1.825 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 0.0 ) == Approx( 0.8 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 6 ) == Approx( 2.25 ).epsilon(0.01) );
+    REQUIRE( graphDataIntrp.getY( 3.75 ) == Approx( 0.5 ).epsilon(0.01) );
+
+    std::shared_ptr< std::vector< std::pair< long double, long double > > > longDouble ( new std::vector< std::pair< long double, long double > > ( { {1.0,9.1}, {2.0,8.2}, {3.0,7.3}, {4.0,6.4}, {5.0,5.5}, {6.0,4.6}, {7.0,3.7}, {8.0,2.8}, {9.0,1.9} }));
+    simpleTools::interpolation <long double, long double> ldIntrp( longDouble );
+    REQUIRE( ldIntrp.getY( 1.5 ) == Approx( 8.65 ));
+
+    std::shared_ptr< std::vector< std::pair< float, float > > > floatData (new std::vector< std::pair< float, float > > ( { {1.0,9.1}, {2.0,8.2}, {3.0,7.3}, {4.0,6.4}, {5.0,5.5}, {6.0,4.6}, {7.0,3.7}, {8.0,2.8}, {9.0,1.9} }));
+    simpleTools::interpolation <float, float> floatIntrp( floatData );
+    REQUIRE( floatIntrp.getY( 1.5 ) == Approx( 8.65 ));
+
+    std::shared_ptr< std::vector< std::pair< double, double > > > emptyData( new std::vector< std::pair< double, double > > ({}));   //empty vector check
+    simpleTools::interpolation <double, double> emptyIntrp( emptyData );
+    REQUIRE( std::isnan( emptyIntrp.getY( 100 )));
+    
+    emptyData->push_back( {1.0, 1.0} );    //cannot work with 1 pair of data
+    simpleTools::interpolation <double, double> singlePair( emptyIntrp );
+    REQUIRE(std::isnan( singlePair.getY( 100 )));
+    
+    std::shared_ptr< std::vector< std::pair< double, double > > > zeroData ( new std::vector< std::pair< double, double > > ( { {0, 0}, {0, 0} }));   //return nan when divide by zero
+    simpleTools::interpolation <double, double> zeroIntrp( zeroData );
+    REQUIRE( std::isnan( zeroIntrp.getY( 1000 )));
+}
